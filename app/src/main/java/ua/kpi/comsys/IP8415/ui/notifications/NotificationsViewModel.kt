b16597b80bb148b4f57a98fc1707a67d5c9d5f90
PaIIadium <vladislavkuchin2001@gmail.com>
2021-02-09 20:54:31 +0200

    Lab 1.1

---
+++ app/src/main/java/ua/kpi/comsys/IP8415/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ua.kpi.comsys.IP8415.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
